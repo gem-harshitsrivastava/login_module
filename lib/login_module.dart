0402f28e0181f51b2a5a65e31eead6b28b8d6778
Harshit Srivastava <harshit.srivastava@geminisolutions.com>
2024-02-06 00:20:01 +0530

    first commit

---
+++ lib/login_module.dart
@@ -0,0 +1,16 @@
+library login_module;
+
+import 'package:flutter/material.dart';
+
+class LoginClass extends StatelessWidget {
+  const LoginClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Logged In Successfully'),
+      ),
+    );
+  }
+}
